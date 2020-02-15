c81895e144adfbfe580443473ca9ff7804d59bc5
killkimno <killkimno@hotmail.com>
2020-02-16 01:13:47 +0900

    1. Yandex 번역기가 제대로 번역하지 못하던 문제 수정.

--- MORT/Properties/Settings.Designer.cs
+++ MORT/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace MORT.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1.191V")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1.192V")]
         public string MORT_VERSION {
             get {
                 return ((string)(this["MORT_VERSION"]));
@@ -53,7 +53,7 @@ namespace MORT.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1191")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1192")]
         public int MORT_VERSION_VALUE {
             get {
                 return ((int)(this["MORT_VERSION_VALUE"]));
@@ -151,7 +151,7 @@ OCR 영역을 빠르게 추가하고 싶을 땐 빠른 OCR 영역을 사용하�
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("2020 01 18")]
+        [global::System.Configuration.DefaultSettingValueAttribute("2020 02 16")]
         public string MORT_RELEASE {
             get {
                 return ((string)(this["MORT_RELEASE"]));
