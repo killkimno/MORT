0df27f0ed49dc634fe4ecf2c9c17f0f41f4f12f5
killkimno <killkimno@hotmail.com>
2020-11-15 17:19:36 +0900

    설정값 변경

--- MORT/Properties/Settings.Designer.cs
+++ MORT/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace MORT.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1.210V")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1.211V")]
         public string MORT_VERSION {
             get {
                 return ((string)(this["MORT_VERSION"]));
@@ -43,7 +43,7 @@ namespace MORT.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1210")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1211")]
         public int MORT_VERSION_VALUE {
             get {
                 return ((int)(this["MORT_VERSION_VALUE"]));
@@ -147,7 +147,7 @@ OCR 영역을 빠르게 추가하고 싶을 땐 빠른 OCR 영역을 사용하�
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("2020 11 14")]
+        [global::System.Configuration.DefaultSettingValueAttribute("2020 11 15")]
         public string MORT_RELEASE {
             get {
                 return ((string)(this["MORT_RELEASE"]));
