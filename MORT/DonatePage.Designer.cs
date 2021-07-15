11e371f171fe12c5ecba311584b7bdf940544f9c
killkimno <killkimno@hotmail.com>
2021-07-15 15:44:57 +0900

    [수정] 오탈자 수정

--- MORT/DonatePage.Designer.cs
+++ MORT/DonatePage.Designer.cs
@@ -139,18 +139,18 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label5.Location = new System.Drawing.Point(170, 89);
+            this.label5.Location = new System.Drawing.Point(185, 89);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(96, 21);
+            this.label5.Size = new System.Drawing.Size(90, 21);
             this.label5.TabIndex = 10;
-            this.label5.Text = "네이버 페이";
+            this.label5.Text = "네이버페이";
             this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("맑은 고딕", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label3.Location = new System.Drawing.Point(110, 41);
+            this.label3.Location = new System.Drawing.Point(122, 41);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(217, 17);
             this.label3.TabIndex = 9;
@@ -161,7 +161,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label2.Location = new System.Drawing.Point(181, 12);
+            this.label2.Location = new System.Drawing.Point(193, 12);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(74, 21);
             this.label2.TabIndex = 8;
