6694d134850b15da25bee56d4632cc874940147d
monkeyhead <killkimno@hotmail.com>
2024-04-14 20:11:12 +0900

    1.277 업데이트

--- MORT/Properties/Settings.Designer.cs
+++ MORT/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace MORT.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1.276V")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1.277V")]
         public string MORT_VERSION {
             get {
                 return ((string)(this["MORT_VERSION"]));
@@ -43,7 +43,7 @@ namespace MORT.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1276")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1277")]
         public int MORT_VERSION_VALUE {
             get {
                 return ((int)(this["MORT_VERSION_VALUE"]));
@@ -90,7 +90,7 @@ OCR 영역을 빠르게 추가하고 싶을 땐 빠른 OCR 영역을 사용하�
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("2024 02 28")]
+        [global::System.Configuration.DefaultSettingValueAttribute("2024 04 14")]
         public string MORT_RELEASE {
             get {
                 return ((string)(this["MORT_RELEASE"]));
