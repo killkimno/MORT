adf859bdf25f6f7c88508af61d758f5df2c90e57
몽키해드 <killkimno@hotmail.com>
2026-07-04 13:43:04 +0900

    파파고가 작동하지 않는 문제 수정

--- MORT/TransAPI/PapagoWebTranslateAPI.cs
+++ MORT/TransAPI/PapagoWebTranslateAPI.cs
@@ -1,30 +1,16 @@
-﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
-using System.Security.Cryptography;
-using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using Windows.Web.Http;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+using Newtonsoft.Json;
 
 namespace MORT.TransAPI
 {
     public class PapagoWebTranslateAPI
     {
         private static readonly string UrlBase = "https://papago.naver.com";
-        private static readonly string UrlN2MT = "/apis/n2mt/translate"; // Neural Machine Translation
-        private static readonly string FormUrlEncodedTemplate = "deviceId={0}&locale=en&dict=false&honorific=false&instant=true&source={1}&target={2}&text={3}";
-
-        private static readonly Guid UUID = Guid.NewGuid();
-
-        private static readonly Regex patternSource = new Regex(@"/main\..*?\.chunk\.js", RegexOptions.Compiled |RegexOptions.Singleline);
-        private static readonly Regex patternVersion = new Regex(@"v\d\.\d\.\d_[^""]+", RegexOptions.Compiled | RegexOptions.Singleline);
-
-        private double _nextUpdate = 0;
-        private string _version; // HMAC 키
+        private static readonly string UrlN2MT = "/api/text/translation";
 
         private string _transCode;
         private string _resultCode;
@@ -39,7 +25,7 @@ namespace MORT.TransAPI
 
         public async Task<(string Result, bool IsError)> TranslateAsync(string text)
         {
-            while(DateTime.Now < _dtNextAvailableTime)
+            while (DateTime.Now < _dtNextAvailableTime)
             {
                 await Task.Delay(50);
                 Console.WriteLine("Wait : " + _dtNextAvailableTime.ToString());
@@ -47,36 +33,18 @@ namespace MORT.TransAPI
 
             System.Net.Http.HttpClient httpClient = new System.Net.Http.HttpClient();
 
-            // 현재 유닉스 밀리 초
-            var timestamp = Math.Truncate(DateTime.Now.Subtract(DateTime.MinValue.AddYears(1969)).TotalMilliseconds);
-
-            // 버전이 없거나 마지막으로 버전 확인하고 10분 지났다면 업데이트하기
-            if(_nextUpdate == 0 || _nextUpdate <= timestamp)
-            {
-                _nextUpdate = timestamp + (60 * 10 * 1000);
-                await updateVersion(httpClient);
-            }
+            httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0");
+            httpClient.DefaultRequestHeaders.Referrer = new Uri(UrlBase + "/");
+            httpClient.DefaultRequestHeaders.Accept.ParseAdd("application/json");
+            httpClient.DefaultRequestHeaders.AcceptLanguage.ParseAdd("en");
 
             var url = UrlBase + UrlN2MT;
-            var param = string.Format(FormUrlEncodedTemplate, UUID, _transCode, _resultCode, text);
-
-            // 버전을 키로 사용한 HmacMD5 으로 인증 토큰 만들기
-            var key = Encoding.UTF8.GetBytes(_version);
-            var data = Encoding.UTF8.GetBytes($"{UUID}\n{url}\n{timestamp}");
-            var token = Convert.ToBase64String(new HMACMD5(key).ComputeHash(data));
-
-            // 최소 헤더 보내기
-            httpClient.DefaultRequestHeaders.Add("Authorization", $"PPG {UUID}:{token}");
-            httpClient.DefaultRequestHeaders.Add("Timestamp", timestamp.ToString());
 
             var paramDic = new Dictionary<string, string>
             {
-                {"Content-Type", "application/x-www-form-urlencoded; charset=UTF-8" },
-                { "deviceId", UUID.ToString() },
-                { "locale", "en" },
                 { "dict", "false" },
                 { "honorific", "false" },
-                { "instant", "true" },
+                { "useGlossary", "false" },
                 { "source", _transCode },
                 { "target", _resultCode },
                 { "text", text }
@@ -86,14 +54,13 @@ namespace MORT.TransAPI
 
             var response = await httpClient.PostAsync(url, encodedContent);
 
-            //랜덤 딜레이를 준다
             double random = _rand.NextDouble();
-            _dtNextAvailableTime = DateTime.Now.AddMilliseconds(random * 850);
+            _dtNextAvailableTime = DateTime.Now.AddMilliseconds(random * 650);
 
-            if(response == null || response.StatusCode != System.Net.HttpStatusCode.OK)
+            if (response == null || response.StatusCode != System.Net.HttpStatusCode.OK)
             {
                 string errorMessage = "Error - null";
-                if(response != null)
+                if (response != null)
                 {
                     errorMessage = $"Error - {response.StatusCode}";
                 }
@@ -103,8 +70,12 @@ namespace MORT.TransAPI
 
             var contentStream = await response.Content.ReadAsStreamAsync();
 
-            var streamReader = new StreamReader(contentStream);
-            var responseText = streamReader.ReadToEnd();
+            string responseText;
+            using (var streamReader = new StreamReader(contentStream))
+            {
+                responseText = streamReader.ReadToEnd();
+            }
+
             string result = "error";
             try
             {
@@ -112,36 +83,16 @@ namespace MORT.TransAPI
 
                 result = pa.TranslatedText;
             }
-            catch(JsonReaderException)
+            catch (JsonReaderException)
             {
                 return ("Invalid JSON", true);
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 return (ex.Message, true);
             }
 
-
             return (result, false);
-        }
-
-        private async Task updateVersion(System.Net.Http.HttpClient wc)
-        {
-            // 메인 페이지에서 버전 불러올 수 있는 자바스크립트 주소 찾아오기
-            var main = await wc.GetStringAsync(UrlBase);
-            var mainMatch = patternSource.Match(main);
-
-            // 자바스크립트 속에서 현재 파파고 버전 확인하기
-            var script = await wc.GetStringAsync(UrlBase + mainMatch.Value);
-            //var scriptMatch = patternVersion.Match(script);
-
-            string pattern = "\"PPG \"(.*)\"(.*?)\"\\).toString";
-            Match match = Regex.Match(script, pattern, RegexOptions.IgnoreCase);
-
-            if(match.Success)
-            {
-                _version = match.Groups[1].Value;
-            }
         }
 
         public void Init(string transCode, string resultCode)
